8dad5e360b0e056cc8e5ce5b60fd1ac3395a01d8
r03181231 <152264010+r03181231@users.noreply.github.com>
2023-12-29 19:22:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    for (let i = 0; i < absolutes.length ; i++ ){
+        if (signs[i] === true ){//absolutes는 실제 양수
+            answer += absolutes[i]
+        }else{//asbsolutes는 실제 음수
+             answer -= absolutes[i]
+        }
+    }
+    return answer;
+
+}
