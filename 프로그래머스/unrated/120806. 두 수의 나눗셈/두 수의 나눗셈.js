03cf36604d8f88ddac709d1510e318ef5b1dd287
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-03 11:04:33 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,8 +1,3 @@
 function solution(num1, num2) {
-    //num1을 num2로 나누고, 1000을 곱한다.
- let answer = (num1/num2)*1000
- // parseInt 숫자 소수점 이하 제거하는 함수를 써준다.
- let number = parseInt((num1/num2)*1000)
-
-    return number;
+  return parseInt((num1/num2)*1000);
 }
