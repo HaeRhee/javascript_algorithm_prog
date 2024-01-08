5cd3aa3cc14c5cf3f0cc9d675a452e417da5e5bd
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-08 11:24:08 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = 0;
     // n % x === 1;
-    // 가장 작은 자연수 x
+    // 가장 작은 자연수 i
     for (let i = 2; i < n; i++) {
          if (n % i === 1) {
          return i;
