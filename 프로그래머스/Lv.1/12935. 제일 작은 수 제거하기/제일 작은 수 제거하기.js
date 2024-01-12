7187ee39cb9f58c3bb0d74b6c2117f5c6795244a
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-12 09:24:15 +0900

    [level Lv.1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let answer = [];
+    let min = Math.min(...arr);
+    
+    if(arr.length <= 1 ){
+      return [-1];
+    }else{
+        arr.splice(arr.indexOf(min),1);
+        answer = arr;
+    }
+    return answer;
+}
