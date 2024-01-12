7837d39e0cf8cdab2d77b44c1004c675b81413c4
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-12 09:13:26 +0900

    [level Lv.1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/Lv.1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -2,9 +2,7 @@ function solution(phone_number) {
     let  answer = ''; //전화번호가 문자열. length. answer: 전화번호
     //전화번호 뒷자리 4자리
     //length - 4
-    let star_push = phone_number.length - 4;
-    let star = "*".repeat(star_push);
+    let star = "*".repeat(phone_number.length - 4);
     let four_num = phone_number.slice(-4, phone_number.length)
-        console.log(four_num)
     return answer = star + four_num;
 }
