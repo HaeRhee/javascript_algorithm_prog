e233d6d4369e3c0e3a3a7598964176bfb257059d
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-15 09:55:12 +0900

    [level Lv.1] Title: 약수의 개수와 덧셈, Time: 0.61 ms, Memory: 35.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,21 @@
+function divisors(num){//약수의 개수
+    let count = 0;
+    for (let i = 1; i <= num; i++){
+        if(num % i === 0 ){
+            count++;
+        }
+    }
+    return count;
+}
+function solution(left, right) {
+   let answer = 0 ;
+    for (let i = left; i <= right; i++){
+        let count = divisors(i);
+        if(count % 2 === 0){
+            answer += i;
+        }else{
+            answer -= i;
+        }
+    }
+    return answer;
+}
