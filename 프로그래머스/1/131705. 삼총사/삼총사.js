52a74b96a734cbe02d32594cdb7cbb2df9806f89
r03181231 <152264010+r03181231@users.noreply.github.com>
2024-01-22 09:23:52 +0900

    [level 1] Title: 삼총사, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,15 @@
+function solution(number) {
+  let answer = 0;
+  // 순서대로 일 때,
+  let sortNum = number.sort();
+  // idx가 0,1,2 더해서 0
+  for (var i = 0; i < sortNum.length - 2; i++) {
+    for (var j = i + 1; j < sortNum.length - 1; j++) {
+      for (var k = j + 1; k < sortNum.length; k++) {
+        if (sortNum[i] + sortNum[j] + sortNum[k] == 0) 
+            answer++;
+      }
+    }
+  }
+  return answer;
+}
