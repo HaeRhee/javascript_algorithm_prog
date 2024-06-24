620c1f6c1972164de3218e89bd596d5548776dfd
South Harry <152264010+HaeRhee@users.noreply.github.com>
2024-06-24 18:01:49 +0900

    [level 0] Title: a와 b 출력하기, Time: 55.03 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -9,10 +9,20 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    const a = Number(input[0])
-    const b = Number(input[1])
-    const printA = `a = ${a}`
-    const printB = `b = ${b}`
-    console.log(printA)
-    console.log(printB)
+   // console.log(input)
+    const [ c, d ] = input
+  //  const a = Number(input[0])
+  //  const b = Number(input[1])
+  //  const printA = `a = ${a}`
+  //  const printB = `b = ${b}`
+  //  console.log(printA)
+  //  console.log(printB)
+  //  console.log(input)
+  //  console.log(a)
+  //  console.log(b)
+  //  console.log(c)
+  //  console.log(d)
+    console.log(`a =`, c)
+    console.log(`b =`, d)
+
 });
