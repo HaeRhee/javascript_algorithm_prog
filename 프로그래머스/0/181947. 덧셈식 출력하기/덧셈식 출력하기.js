9b8b877fbb2f20f564835eb1f3c6ffe03640e3a1
South Harry <152264010+HaeRhee@users.noreply.github.com>
2024-09-06 17:49:42 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 58.25 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -8,9 +8,9 @@ let input = [];
 
 rl.on('line', function (line) {
     let input = line.split(' ').map(Number);
-    const a = input[0]
-    const b = input[1]
-    const c = a + b
-    console.log(`${a} + ${b} = ${c}`)
+    const a = input[0];
+    const b = input[1];
+    const c = a + b;
+    console.log(`${a} + ${b} = ${c}`);
     rl.close();
 })
