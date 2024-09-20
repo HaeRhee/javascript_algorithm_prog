1e73b9db742a058ac3e698a450e3897912f0df95
South Harry <152264010+HaeRhee@users.noreply.github.com>
2024-09-20 18:27:49 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.82 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,7 +1,11 @@
 function solution(arr, queries) {
     const result = [];
     
-    for(const [s, e, k] of queries) {
+    for(const query of queries) {
+        const s = query[0]
+        const e = query[1]
+        const k = query[2] 
+        
         const subArray = arr.slice(s, e + 1);
         
         const filtered = subArray.filter(x => x > k);
