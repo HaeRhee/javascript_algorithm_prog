0bd3615a63629239a1ce2754e408eb1318de9f7d
South Harry <152264010+HaeRhee@users.noreply.github.com>
2024-10-04 17:43:06 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 9.46 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,6 +1,5 @@
 function solution(number) {
-    // let sum = number.split("").reduce((acc, accNumber) => acc + parseInt(accNumber), 0);
-    let sum = 0;
+   let sum = 0;
     
    number.split("").forEach((currNumber) => {
         sum += parseInt(currNumber);
