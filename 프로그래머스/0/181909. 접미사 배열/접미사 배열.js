838c9bcc2b38ff401644c92432c563d793c7c4c1
South Harry <152264010+HaeRhee@users.noreply.github.com>
2024-10-07 18:46:22 +0900

    [level 0] Title: 접미사 배열, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let suffixes = [];
+    
+    for (let i = 0; i < my_string.length; i++) {
+        suffixes.push(my_string.slice(i));
+    }
+    
+    suffixes.sort();
+    
+    return suffixes;
+}
